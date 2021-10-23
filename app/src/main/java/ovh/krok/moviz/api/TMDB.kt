7977d9874ca90254e6343b395e4e5e43aceb9cbc
Simon LEONARD <git-1001af4@sinux.sh>
2021-10-23 15:39:42 +0200

    TMDB.search passe une liste de movie à la callback. Un movie à une description

--- app/src/main/java/ovh/krok/moviz/api/TMDB.kt
+++ app/src/main/java/ovh/krok/moviz/api/TMDB.kt
@@ -5,20 +5,23 @@ import com.android.volley.Response
 import org.json.JSONArray
 import org.json.JSONObject
 import ovh.krok.moviz.api.utility.APIGeneric
+import ovh.krok.moviz.model.Movie
+import java.util.ArrayList
 
 class TMDB(context: Context) : APIGeneric(context, API_URL, API_TOKEN){
 
     companion object {
         private const val API_URL = "https://api.themoviedb.org/3"
+        private const val API_ASSET = "https://image.tmdb.org/t/p/w500"
         // It's a read-only token, don't bother
         private const val API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOTYyNTRlMTgwNWRlYTIxNDU2ZWNhY2I3M2I5NGNmNyIsInN1YiI6IjYxNzExMDBmNmQ0Yzk3MDAyNzI5NTU3NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MafTX6qNT7MEB8hHmjPHuG_XViaqzrQ7VREC_YmGj-o"
     }
 
 
-    fun search(query: String, callback: (JSONArray) -> Unit) {
+    fun search(query: String, callback: (List<Movie>) -> Unit) {
         val params: HashMap<String, String> = hashMapOf()
         params["language"] = "fr" // @TODO: get lang of system
-        params["query"] = query.replace(" ", "+")
+        params["query"] = query.trim().replace(" ", "+")
 
 
         // Pas compris pourquoi la callback devait être en dehors des arguments, mais voici :
@@ -28,7 +31,19 @@ class TMDB(context: Context) : APIGeneric(context, API_URL, API_TOKEN){
             params
         ) { response ->
             val obj = response.getJSONArray("results")
-            callback(obj)
+
+            val movies: ArrayList<Movie> = arrayListOf()
+            for (i in 0 until obj.length()) {
+                val movieJson = obj.getJSONObject(i)
+                val movie = Movie(
+                    movieJson.getString("original_title") ,
+                    API_ASSET + movieJson.getString("poster_path"),
+                    movieJson.getString("overview")
+                )
+                movies.add(movie)
+            }
+
+            callback(movies)
         }
     }
 }
--- app/src/main/java/ovh/krok/moviz/model/Movie.kt
+++ app/src/main/java/ovh/krok/moviz/model/Movie.kt
@@ -1,11 +1,17 @@
 package ovh.krok.moviz.model
 
+import org.json.JSONObject
 import java.io.Serializable
 
 class Movie(val titre : String,
-            val image_url : String
-):Serializable{
+            val image_url : String,
+            val description : String )
+    : Serializable {
+
+
     override fun toString(): String {
         return "$titre , $image_url"
     }
+
+
 }
